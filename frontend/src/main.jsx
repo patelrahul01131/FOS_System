db2b5820d705ed6feb94ff46534a5f66d763ff3c
Patelrahul3501 <95459680+Patelrahul3501@users.noreply.github.com>
2026-01-20 10:36:35 +0530

    Setup: Convert website to PWA for Satnam Sales

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,3 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(reg => console.log('Service Worker registered!', reg))
+      .catch(err => console.log('Service Worker failed:', err));
+  });
+}
